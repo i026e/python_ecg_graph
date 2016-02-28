9dc4416cc73213577e14e4aca7bdd7ccf2fd3ff0
Pavel <klev.paul@gmail.com>
2016-02-28 16:38:20 +0300

    Signal inversion

--- data_processing.py
+++ data_processing.py
@@ -19,7 +19,7 @@ class CircularBuffer:
             self.size = size
 
             self.pointer = 0
-            
+
         def is_full(self):
             return self.full
         def write(self, val):
@@ -43,7 +43,7 @@ class CircularBuffer:
             return self.array[index]
         def all_entries(self):
             """get data as it was added (FIFO)"""
-            
+
             #data stored in reversed order
             index = self.pointer - 1
             while index >= 0:
@@ -54,93 +54,93 @@ class CircularBuffer:
                 while index >= self.pointer:
                     yield self.array[index]
                     index -= 1
-            
+
 class Filter:
     #http://dsp.stackexchange.com/a/25679
     def __init__(self, fs, f, r):
         """fs = sampling frequency
             f = notch frequency
             r = sharpness, 0..1 excluding 1
-        """        
+        """
         z1x = math.cos(2*math.pi*f/fs)
         self.a0a2 = (1.0 - r)*(1.0 - r)/(2.0*(abs(z1x) + 1.0)) + r
         self.a1 = -2.0*z1x*self.a0a2
         self.b1 = 2.0*z1x*r
         self.b2 = -r*r
-        
+
         self.in_buffer = CircularBuffer(2)
         self.out_buffer = CircularBuffer(2)
-        
+
     def filtered(self, val):
         """ apply filter to value"""
         new_val = val
         if self.out_buffer.is_full():
             new_val = ( self.a0a2*(val + self.in_buffer.get_entry(2)) +
-                        self.a1*self.in_buffer.get_entry(1) + 
-                        self.b1*self.out_buffer.get_entry(1) + 
+                        self.a1*self.in_buffer.get_entry(1) +
+                        self.b1*self.out_buffer.get_entry(1) +
                         self.b2*self.out_buffer.get_entry(2) )
-                
-            
+
+
         self.in_buffer.write(val)
         self.out_buffer.write(new_val)
         return new_val
-        
+
 class AdjustableFilter:
     """posible to change parameters of filter"""
-    
+
     def __init__(self, freq, roughness):
         self.freq = freq
         self.roughness = roughness
-        
+
         self.sampl_freq = 1.0 # not yet known
         self.sampl_freq_known = False
         self.countdown = settings.SAMPLS_TO_MEASURE_SAMPL_RATE
         self.measure_start_time = 0
-        
+
         self.in_buffer = CircularBuffer(2)
         self.out_buffer = CircularBuffer(2)
-        
+
     def filtered(self, val):
         new_val = val
         if self.out_buffer.is_full() and self.sampl_freq_known:
             new_val = ( self.a0a2*(val + self.in_buffer.get_entry(2)) +
-                        self.a1*self.in_buffer.get_entry(1) + 
-                        self.b1*self.out_buffer.get_entry(1) + 
+                        self.a1*self.in_buffer.get_entry(1) +
+                        self.b1*self.out_buffer.get_entry(1) +
                         self.b2*self.out_buffer.get_entry(2) )
             #print(val, new_val)
-            
+
         else:
             #measure time for SAMPLES_TO_MEASURE_FREQ samples
             if self.countdown == settings.SAMPLS_TO_MEASURE_SAMPL_RATE:
                 self.measure_start_time = time.time()
             elif self.countdown <= 0:
                 self._calc_sampl_freq()
-                
-            self.countdown -= 1    
+
+            self.countdown -= 1
         self.in_buffer.write(val)
         self.out_buffer.write(new_val)
         return int(round(new_val))
-        
+
     def set_roughness(self, roughness):
         self.roughness = roughness
         self._init_values()
-    
+
     def get_freq(self):
         return self.freq
     def set_freq(self, freq):
         self.freq = freq
         self._init_values()
-        
+
     def reset_sampling_freq(self):
         self.sampl_freq_known = False
         self.countdown = settings.SAMPLS_TO_MEASURE_SAMPL_RATE
-        
+
     def _calc_sampl_freq(self):
         self.sampl_freq_known = True
         time_interval = time.time() - self.measure_start_time # seconds
         print("time for " + str(settings.SAMPLS_TO_MEASURE_SAMPL_RATE) +
                  " samples : " + str(time_interval) + " seconds")
-        self.sampl_freq = (settings.SAMPLS_TO_MEASURE_SAMPL_RATE 
+        self.sampl_freq = (settings.SAMPLS_TO_MEASURE_SAMPL_RATE
                                                     / time_interval) # Hz
         self._init_values()
     def _init_values(self):
@@ -149,76 +149,87 @@ class AdjustableFilter:
         self.a1 = -2.0*z1x*self.a0a2
         self.b1 = 2.0*z1x*self.roughness
         self.b2 = -self.roughness*self.roughness
-        
-        
+
+
 class DataProcessor:
-    def __init__(self, output_graph, valid_color_name, error_color_name, 
-                 do_filtering, filter_freq, filter_roughness): 
+    def __init__(self, output_graph, valid_color_name, error_color_name,
+                 do_filtering, filter_freq, filter_roughness, do_inversion):
         """
-        do_filtering: boolean should filter be used  
-        filter_freq: frequency to supress        
+        do_filtering: boolean should filter be used
+        filter_freq: frequency to supress
         filter_roughness: filter strength in [0,1)
-        
+
         output_graph: Graph obj
         valid_color_name = color name for valid data
-        error_color_name = color name for invalid data 
+        error_color_name = color name for invalid data
         """
         self.graph = output_graph
         self.val_col_nm = valid_color_name
         self.err_color_nm = error_color_name
-        
-        self.data_to_export = CircularBuffer(settings.NUM_ENTRIES_TO_EXPORT)      
-        
+
+        self.data_to_export = CircularBuffer(settings.NUM_ENTRIES_TO_EXPORT)
+
         self.lastX = self.lastY = 0
         self.samples_before_plotting = settings.NUM_ENTRIES_BEFORE_PLOT
-        
+
         self.busy_flag = False
         self.filtering_flag = do_filtering
-        
+        self.inversion_flag = do_inversion
+
         self.filter = AdjustableFilter(filter_freq, filter_roughness)
-        
+
     def new_data(self, data):
         """ data : string of from input """
-        
+
         if data is not None and not self.busy_flag:
         # process only if real data
         # skip data if busy
             self.busy_flag = True
-            
+
             line = data.strip()
-            if len(line) > 0:                
+            if len(line) > 0:
                 try:
                     #integer value received
                     y = int(line)
-                    
+
+                    #data inversion
+                    if self.inversion_flag:
+                        y = settings.DATA_MAX_VALUE - y
+
+                    #data filtering
                     if self.filtering_flag:
                         y = self.filter.filtered(y)
-                        
-                    self.graph.add_line(self.val_col_nm, 
-                                        self.lastX, self.lastY, 
+
+                    # send to plot
+                    self.graph.add_line(self.val_col_nm,
+                                        self.lastX, self.lastY,
                                         self.lastX+1, y)
                     self.lastY = y
+                    # append to export buffer
                     self.data_to_export.write(y)
-                except ValueError:                        
+                except ValueError:
                     # non integer value
-                    self.graph.add_line(self.err_color_nm, 
-                                    self.lastX, self.lastY, 
+                    self.graph.add_line(self.err_color_nm,
+                                    self.lastX, self.lastY,
                                     self.lastX+1, self.lastY)
                 except:
                     print("unexpected value " + str(y))
-            
+
             self.lastX += 1
             self.samples_before_plotting -= 1
-            
+
             if (self.samples_before_plotting <= 0):
                 self.redraw_graph()
-                
+
             self.busy_flag = False
-    def enable_filter(self):
-        self.filtering_flag = True
-    def disable_filter(self):
-        self.filtering_flag = False
-        
+    def set_filtering(self, allow_filter):
+        # allow_filter : True - use filter
+        #                False - do not use filter
+        self.filtering_flag = allow_filter
+    def set_inversion(self, allow_inversion):
+        # allow_inversion : True - invert signal
+        self.inversion_flag = allow_inversion
+
     def get_filter_freq(self):
         return self.filter.get_freq()
     def set_filter_freq(self, new_freq):
@@ -227,11 +238,11 @@ class DataProcessor:
         self.filter.set_roughness(roughness)
     def reset_filter_sampl_freq(self):
         self.filter.reset_sampling_freq()
-        
+
     def redraw_graph(self):
         self.samples_before_plotting = settings.NUM_ENTRIES_BEFORE_PLOT
         self.graph.update_screen()
-        
+
         if self.lastX > self.graph.get_width():
             self.lastX = 0
             self.graph.clear_screen()
@@ -239,11 +250,11 @@ class DataProcessor:
         while self.busy_flag:
             time.sleep(settings.SLEEP_INTERVAL)
         self.busy_flag = True
-        
+
         # create new circular buffer
         tmp_data = self.data_to_export
         self.data_to_export = CircularBuffer(settings.NUM_ENTRIES_TO_EXPORT)
-        self.busy_flag = False      
+        self.busy_flag = False
 
         return tmp_data
     def disable_accepting_data(self):
@@ -256,28 +267,27 @@ class FileProcessor:
     def __init__(self):
         date = datetime.now()
         self.filename = (date.strftime(settings.FILENAME_DATE_FORMAT) +
-                                        settings.FILENAME_EXTENSION)  
+                                        settings.FILENAME_EXTENSION)
     def get_name(self):
         return self.filename
     def set_name(self, new_name):
         self.filename = new_name
         print("new filename: " + new_name)
     def do_export(self, data_processor, onError):
-        try:            
+        try:
             f_obj = open(self.filename, 'w')
             circ_buffer = data_processor.get_export_data()
-            
+
             index = 0
             for entry in circ_buffer.all_entries():
                 line = str(index) + settings.CSV_SEPARATOR + str(entry)
                 f_obj.write(line + settings.CSV_LINE_TERMINATOR)
                 index += 1
-            
+
             f_obj.close()
-            
+
         except Exception as e:
             f_obj.close()
             onError(str(e))
-        
-        
-        
+
+
--- ecg.py
+++ ecg.py
@@ -23,180 +23,185 @@ import settings
 class GUI:
     GLADE_FILE = "ECG.glade"
     EXPORT_RESPONSE_OK = 1
-    
+
     def __init__(self):
         self.builder = Gtk.Builder()
         self.builder.add_from_file(GUI.GLADE_FILE)
-        
+
         self._gui_elements_init_()
-        
+
         self._ports_init_()
         self._graph_init_()
         self._data_procesor_init_()
-        
-        
+
+
         self.is_active = False
-        
+
         self.builder.connect_signals(self)
-        
+
         self.builder.get_object("main_window").show()
-        
+
         self.stop()
         #
     def _gui_elements_init_(self):
         #filter settings
-        self.gui_filter_chkbox = self.builder.get_object("filter_button")        
+        self.gui_filter_chkbox = self.builder.get_object("filter_button")
         self.gui_freq_txtbox = self.builder.get_object("freq_entry")
         self.gui_filter_rough_adj = self.builder.get_object("filter_adj")
+        self.gui_invert_chkbox = self.builder.get_object("invert_button")
+
 
         self.gui_filter_chkbox.set_active(settings.USE_FILTER)
+        self.gui_invert_chkbox.set_active(settings.INVERT_SIGNAL)
         self.gui_freq_txtbox.set_text(str(settings.FILTER_FREQ))
-        self.gui_filter_rough_adj.set_value(settings.FILTER_INV_STRENGTH)        
-        
-        #start button         
+        self.gui_filter_rough_adj.set_value(settings.FILTER_INV_STRENGTH)
+
+        #start button
         self.gui_start_btn = self.builder.get_object("start_btn")
         self.gui_start_label = self.builder.get_object("start_lbl")
         self.gui_stop_label = self.builder.get_object("stop_lbl")
-        
+
         #port settings
         self.gui_settings_area = self.builder.get_object("settings_alignment")
         # combobox with serial ports
         self.gui_serial_ports_list = self.builder.get_object("serial_ports")
         #combobox with baudrates
         self.gui_baudrates_list = self.builder.get_object("baudrates")
-        
-        
+
+
         #export dialog
         self.gui_export_btn = self.builder.get_object("export_btn")
         self.gui_export_dialog = None
-        
+
         #error message
         self.gui_error_message_dialog = None
-        
+
     def _ports_init_(self):
         self.serial_port = SerialPort()
-        
-        
+
+
         for port in self.serial_port.enumerate_port_names():
-            self.gui_serial_ports_list.append_text(port)           
-            
+            self.gui_serial_ports_list.append_text(port)
+
         for baudrate in settings.BAUDRATES:
             self.gui_baudrates_list.append_text(str(baudrate))
-         
-        # compute index of port sected by default 
+
+        # compute index of port sected by default
         # first try by name , no guarantee
         selected_port = self.serial_port.get_prefered_port_ind()
         if selected_port is None:
             #next by index
             selected_port = self.serial_port.get_default_port_ind()
-            
+
         self.gui_serial_ports_list.set_active(selected_port)
-        
+
         self.gui_baudrates_list.set_active(settings.DEFAULT_BAUDRATE_IND)
         self.on_serial_port_changed()
         self.on_baudrate_changed()
-        
+
     def _graph_init_(self):
-        # Create graph   
+        # Create graph
         #self.graph = MatplotlibGraph(onClose=self.stop)
         error_color = GTK_Graph.Color(settings.ERROR_COLOR_RGB)
         valid_color = GTK_Graph.Color(settings.VALID_COLOR_RGB)
-        
-        colors = {settings.ERROR_COLOR_NAME:error_color, 
+
+        colors = {settings.ERROR_COLOR_NAME:error_color,
                   settings.VALID_COLOR_NAME:valid_color}
-        self.graph = GTK_Graph(self.builder.get_object("graph_area"), 
+        self.graph = GTK_Graph(self.builder.get_object("graph_area"),
                                colors, settings.DATA_MIN_VALUE, settings.DATA_MAX_VALUE)
-        
+
     def _data_procesor_init_(self):
-        filter_enable = self.gui_filter_chkbox.get_active()
-        roughness = self.gui_filter_rough_adj.get_value()
-        frequency = int(self.gui_freq_txtbox.get_text())
-        self.data_processor = DataProcessor(output_graph = self.graph, 
+        self.data_processor = DataProcessor(output_graph = self.graph,
                                             valid_color_name = settings.VALID_COLOR_NAME,
                                             error_color_name = settings.ERROR_COLOR_NAME,
-                                            do_filtering = filter_enable,
-                                            filter_freq = frequency,
-                                            filter_roughness = roughness)
-                 
-                                            
+                                            do_filtering = settings.USE_FILTER,
+                                            filter_freq = settings.FILTER_FREQ,
+                                            filter_roughness = settings.FILTER_INV_STRENGTH,
+                                            do_inversion = settings.INVERT_SIGNAL)
+
+
     def close(self):
         self.stop()
-        
+
     def start(self):
         if self.is_active:
             self.stop()
-        
+
         print("start")
         self.is_active = True
-        
+
         # handling with gui first!!!
-        
+
         #disable port settings
         self.gui_settings_area.set_sensitive(False)
-        #rename start button        
+        #rename start button
         self.gui_start_btn.set_label(self.gui_stop_label.get_text())
         #disable export button
         self.gui_export_btn.set_sensitive(False)
-        
 
-        self.data_processor.enable_accepting_data()                
+
+        self.data_processor.enable_accepting_data()
         # start listening
         self.serial_port.activate(onAnswer=self.data_processor.new_data,
                                   onError = self.error_stop)
-    
+
     def error_stop(self, text):
         self.error_message(text)
         self.stop()
-        
+
     def error_message(self, text):
         print text
         if self.gui_error_message_dialog is None:
             self.gui_error_message_dialog = self.builder.get_object("error_message_dialog")
-            
+
         self.gui_error_message_dialog.set_property("secondary-text", text)
         self.gui_error_message_dialog.run()
         self.gui_error_message_dialog.hide()
-        
+
     def stop(self):
         print("stop")
         self.serial_port.deactivate()
         self.data_processor.disable_accepting_data()
-        
+
         self.is_active = False
-        
-        #enable settings        
+
+        #enable settings
         self.gui_settings_area.set_sensitive(True)
-        #rename start button        
+        #rename start button
         self.gui_start_btn.set_label(self.gui_start_label.get_text())
         #enable export button
-        self.gui_export_btn.set_sensitive(True)       
-        
-        
-    def on_main_window_delete_event(self, *args):        
+        self.gui_export_btn.set_sensitive(True)
+
+
+    def on_main_window_delete_event(self, *args):
         self.close()
         Gtk.main_quit()
-    
+
     def on_serial_port_changed(self, *args):
         self.serial_port.set_port(self.gui_serial_ports_list.get_active_text())
-    def on_baudrate_changed(self, *args):  
+    def on_baudrate_changed(self, *args):
         self.serial_port.set_rate(self.gui_baudrates_list.get_active_text())
-        
+
     def on_start_clicked(self, *args):
         if self.is_active:
             self.stop()
         else:
             self.start()
-    def on_filter_toggled(self, *args):        
-        #print(dir(self.filter_chkbox)) 
-        if self.gui_filter_chkbox.get_active():
-            self.data_processor.enable_filter()
-        else:
-            self.data_processor.disable_filter()
+    def on_invert_toggled(self, *args):
+        #print(dir(self.filter_chkbox))
+        self.data_processor.set_inversion(
+                                self.gui_invert_chkbox.get_active())
+
+        print("inversion changed to " + str(self.gui_invert_chkbox.get_active()))
+    def on_filter_toggled(self, *args):
+        #print(dir(self.filter_chkbox))
+        self.data_processor.set_filtering(
+                self.gui_filter_chkbox.get_active())
         print("filtering changed to " + str(self.gui_filter_chkbox.get_active()))
     def on_freq_changed(self, *args):
         #print dir(self.gui_freq_txtbox)
         str_val = self.gui_freq_txtbox.get_text()
-        try:            
+        try:
             val = 0 # treat empty string as implicit 0
             if str_val != "":
                 val = int(str_val)
@@ -210,24 +215,24 @@ class GUI:
     def on_export_clicked(self, *args):
         if self.gui_export_dialog is None:
             self.gui_export_dialog = self.builder.get_object("export_filechooser_dialog")
-            
-        fproc = FileProcessor()   
+
+        fproc = FileProcessor()
         self.gui_export_dialog.set_current_name(fproc.get_name())
-        
+
         response = self.gui_export_dialog.run()
-        
+
         if response == gui.EXPORT_RESPONSE_OK:
             fproc.set_name(self.gui_export_dialog.get_filename())
             fproc.do_export(self.data_processor, onError=self.error_message)
-            
+
         self.gui_export_dialog.hide()
-        
-    def on_filter_rough_value_changed(self, *args): 
+
+    def on_filter_rough_value_changed(self, *args):
         #print(dir(self.gui_filter_rough_adj))
         val = self.gui_filter_rough_adj.get_value()
         self.data_processor.set_filter_roughness(val)
         print("roughness " + str(val))
 
 if __name__ == "__main__":
-    gui = GUI()   
+    gui = GUI()
     Gtk.main()
